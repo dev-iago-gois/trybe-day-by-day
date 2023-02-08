e354ec67d6fb44750ccaf8498cff2445965a6a2d
Iago Gois <iagorafaelbg@gmail.com>
2023-02-08 16:01:25 -0300

    adicionada dependencia de copy to clipboard ao clicar em cima da senha random gerada. TODO adicionar icone de click

--- 02-front-end/1-/dia-1/gerador-de-senhas/src/main.js
+++ 02-front-end/1-/dia-1/gerador-de-senhas/src/main.js
@@ -1,9 +1,15 @@
 import { nanoid } from 'nanoid';
-
-const passwordBtnEl = document.getElementById('password-btn')
-passwordBtnEl.addEventListener('click', () => {
-  const generateRandomPassword = nanoid();
-  displayPasswordEl.innerHTML = generateRandomPassword;
-});
+import copy from 'clipboard-copy';
+import './style.css';
 
 const displayPasswordEl = document.getElementById('display-password');
+const passwordBtnEl = document.getElementById('password-btn');
+passwordBtnEl.addEventListener('click', () => {
+    const generateRandomPassword = nanoid();
+    displayPasswordEl.innerHTML = generateRandomPassword;
+});
+
+displayPasswordEl.addEventListener('click', (event) => {
+    copy(event.target.innerHTML);
+    alert('Senha Copiada!');
+});
